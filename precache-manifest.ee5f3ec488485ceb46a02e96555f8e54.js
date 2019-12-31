e58fc534be941a52676bcdfd2c6f7d7b84c55575
Ashley D <webdevmvp@gmail.com>
2019-12-31 23:37:39 +0000

    Updates

---
+++ precache-manifest.ee5f3ec488485ceb46a02e96555f8e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710fe32bd83608b34ac8caba4046814b",
+    "url": "/react-lyrics/index.html"
+  },
+  {
+    "revision": "4fb8f8491991d80a0cd1",
+    "url": "/react-lyrics/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c7745d2841584a309953",
+    "url": "/react-lyrics/static/js/2.9d56b07e.chunk.js"
+  },
+  {
+    "revision": "2565e368e1ca58a1e4bb8ec25c22c912",
+    "url": "/react-lyrics/static/js/2.9d56b07e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fb8f8491991d80a0cd1",
+    "url": "/react-lyrics/static/js/main.b1e6e42d.chunk.js"
+  },
+  {
+    "revision": "133c6b4981648fc08b88",
+    "url": "/react-lyrics/static/js/runtime-main.8b27324b.js"
+  }
+]);
